9942858320e5dcc0368529fdd48ce4a650b38661
deJames-13 <de.james013@gmail.com>
2024-03-28 15:06:28 +0800

    bvcb

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,36 +1,35 @@
 <x-app-layout>
-    <div>
+	<div>
 
-        <!--Page Title-->
-        <section class="w-full flex justify-center py-8 border-b-2">
-            <div class='prose'>
-                <h1>
-                    RENT APP
-                </h1>
-            </div>
-        </section>
-        <!--End Page Title-->
-        <x-card class="mx-auto my-6 container max-w-xl">
-            <div role="tablist" class="tabs tabs-boxed">
-                <x-button link='/login' role="tab"
-                    class="tab btn-sm {{ $active === 'login' ? 'tab-active' : '' }}">Login</x-button>
-                <x-button link='/register'
-                    class="tab btn-sm {{ $active === 'register' ? 'tab-active' : '' }}">Register</x-button>
-            </div>
-            @switch($active)
-                @case('register')
-                    <livewire:register />
-                @break
+		<!--Page Title-->
+		<section class="w-full flex justify-center py-8 border-b-2">
+			<div class='prose'>
+				<x-button link="/" class="font-extra-bold text-3xl btn-ghost bg-none border-none">
+					RENT APP
+				</x-button>
+			</div>
+		</section>
+		<!--End Page Title-->
+		<x-card class="mx-auto my-6 container max-w-xl">
+			<div role="tablist" class="tabs tabs-boxed">
+				<x-button link='/login' role="tab"
+					class="tab btn-sm {{ $active === 'login' ? 'tab-active' : '' }}">Login</x-button>
+				<x-button link='/register' class="tab btn-sm {{ $active === 'register' ? 'tab-active' : '' }}">Register</x-button>
+			</div>
+			@switch($active)
+				@case('register')
+					<livewire:register />
+				@break
 
-                @case('login')
-                    <livewire:login />
-                @break
+				@case('login')
+					<livewire:login />
+				@break
 
-                @default
-            @endswitch
-
+				@default
+			@endswitch
 
 
-        </x-card>
-    </div>
+
+		</x-card>
+	</div>
 </x-app-layout>
