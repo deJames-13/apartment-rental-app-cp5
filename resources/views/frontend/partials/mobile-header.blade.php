96b1790fce34d5803706b5596786d9da50ab52a3
deJames-13 <de.james013@gmail.com>
2024-03-28 15:56:12 +0800

    dvvads

--- resources/views/frontend/partials/mobile-header.blade.php
+++ resources/views/frontend/partials/mobile-header.blade.php
@@ -5,15 +5,15 @@
 		<path d="M4 6h16M4 12h16M4 18h7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
 	</svg>
 </button>
-<div class="menu menu-sm z-[1] mt-3 hidden w-52 rounded-box bg-base-100 p-2 shadow" id="multi-dropdown" tabindex="0">
+<div class="menu menu-sm z-[1] mt-3  w-52 rounded-box bg-base-100 p-2 shadow" id="multi-dropdown" tabindex="0">
 	<ul aria-labelledby="multiLevelDropdownButton" class="py-2 text-sm text-gray-700 dark:text-gray-200">
 		@foreach ($navItems as $index => $item)
-			@if (isset($item["submenu"]))
+			@if (isset($item['submenu']))
 				<li>
 					<button
 						class="flex w-full items-center justify-between px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
 						data-dropdown-placement="right-start" data-dropdown-toggle="doubleDropdown{{ $index }}"
-						id="doubleDropdownButton{{ $index }}" type="button">{{ $item["label"] }}<svg aria-hidden="true"
+						id="doubleDropdownButton{{ $index }}" type="button">{{ $item['label'] }}<svg aria-hidden="true"
 							class="ms-3 h-2.5 w-2.5 rtl:rotate-180" fill="none" viewBox="0 0 6 10" xmlns="http://www.w3.org/2000/svg">
 							<path d="m1 9 4-4-4-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" />
 						</svg>
@@ -22,16 +22,16 @@
 						id="doubleDropdown{{ $index }}">
 						<ul aria-labelledby="doubleDropdownButton{{ $index }}"
 							class="py-2 text-sm text-gray-700 dark:text-gray-200">
-							@foreach ($item["submenu"] as $subIndex => $subItem)
+							@foreach ($item['submenu'] as $subIndex => $subItem)
 								<li id="submenuItem{{ $index }}_{{ $subIndex }}"><a
 										class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-										href="{{ $subItem["link"] }}">{{ $subItem["label"] }}</a></li>
+										href="{{ $subItem['link'] }}">{{ $subItem['label'] }}</a></li>
 							@endforeach
 						</ul>
 					</div>
 				</li>
 			@else
-				<li id="singleItem{{ $index }}"><a href="{{ $item["link"] }}">{{ $item["label"] }}</a></li>
+				<li id="singleItem{{ $index }}"><a href="{{ $item['link'] }}">{{ $item['label'] }}</a></li>
 			@endif
 		@endforeach
 	</ul>
