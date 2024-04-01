244a97d72e6338ec812de1aab934f906fbc47c89
deJames-13 <de.james013@gmail.com>
2024-04-01 22:37:48 +0800

    x

--- app/Livewire/SearchBar.php
+++ app/Livewire/SearchBar.php
@@ -6,23 +6,44 @@ use Livewire\Component;
 
 class SearchBar extends Component
 {
-  public $active = 'property';
+  public $active = 'units';
+  public $search = '';
+  public $location = '';
   public $unit_types = ['Apartment', 'Condominium', 'House', 'Townhouse', 'Commercial', 'Industrial'];
   public $property_types = ['Apartment', 'Condominium', 'House', 'Townhouse', 'Commercial', 'Industrial'];
   public $selected_ptype = '';
   public $selected_utype = '';
-  public $unitLocation = '';
-  public $propLocation = '';
 
   public function render()
   {
     return view('frontend.partials.search-bar');
   }
 
-  public function searchProperty()
+
+  public function search()
   {
+    if ($this->active === 'units') {
+      return redirect()->route('search.units', [
+        'search' => $this->search,
+        'unit_type' => $this->selected_utype,
+        'location' => $this->unitLocation,
+
+      ]);
+    }
+    if ($this->active === 'properties') {
+      return redirect()->route('search.properties', [
+        'search' => $this->search,
+        'property_type' => $this->selected_ptype,
+        'location' => $this->propLocation
+      ]);
+    }
   }
-  public function unitProperty()
+  public function setToUnit()
   {
+    $this->active = 'units';
+  }
+  public function setToProperties()
+  {
+    $this->active = 'properties';
   }
 }
--- resources/views/frontend/partials/search-bar.blade.php
+++ resources/views/frontend/partials/search-bar.blade.php
@@ -2,12 +2,12 @@
 	<div class="tabs-md tabs w-1/5 gap-1 text-center md:tabs-lg lg:mx-auto" role="tablist">
 		<a :class="{ 'tab-active bg-secondary text-white transition-all duration-300 ease-in-out ': activeTab !== 'Tab 1' }"
 			@click="activeTab = 'Tab 1'" class="tab rounded-t-lg bg-base-100 font-bold transition-all ease-in hover:bg-primary"
-			role="tab">
+			role="tab" wire:click='setToProperties'>
 			Properties
 		</a>
 		<a :class="{ 'tab-active bg-secondary text-white transition-all duration-300 ease-in-out ': activeTab !== 'Tab 2' }"
 			@click="activeTab = 'Tab 2'" class="tab rounded-t-lg bg-base-100 font-bold transition-all ease-in hover:bg-primary"
-			role="tab">
+			role="tab" wire:click='setToUnit'>
 			Units
 		</a>
 	</div>
@@ -37,8 +37,8 @@
 		<div class="animate__animated animate__zoomIn container w-full p-8" x-show="activeTab === 'Tab 2'">
 			<div class="grid w-full grid-cols-1 items-end gap-3 lg:grid-cols-3 lg:gap-6">
 
-				<x-input icon="o-magnifying-glass" label="Search Unit" placeholder="Search by unit name" />
-				<x-input icon="o-magnifying-glass" label="Search Location" placeholder="Search by location" />
+				<x-input icon="o-magnifying-glass" wire:model='' label="Search Unit" placeholder="Search by unit name" />
+				<x-input icon="o-magnifying-glass" wire:model='' label="Search Location" placeholder="Search by location" />
 
 
 				<div class="flex flex-col space-y-2 text-sm font-bold">
@@ -51,7 +51,7 @@
 				</div>
 			</div>
 		</div>
-		<div
+		<div wire:click="search"
 			class="hover:bg-btn-secondary btn-outline flex items-center justify-center space-x-2 rounded-b-lg bg-secondary bg-button-gradient bg-200% p-4 px-8 font-bold text-black transition-all duration-500 ease-in-out hover:bg-right hover:text-white lg:rounded-l-none lg:rounded-r-lg">
 			<x-heroicon-o-magnifying-glass class="h-6 w-6" />
 			<span class="">Search</span>
