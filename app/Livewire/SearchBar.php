01d84a9507a01272fd2ff31d11b3188d2e283b98
deJames-13 <de.james013@gmail.com>
2024-04-01 22:03:45 +0800

    dsaf

--- app/Livewire/SearchBar.php
+++ app/Livewire/SearchBar.php
@@ -6,17 +6,23 @@ use Livewire\Component;
 
 class SearchBar extends Component
 {
+  public $active = 'property';
   public $unit_types = ['Apartment', 'Condominium', 'House', 'Townhouse', 'Commercial', 'Industrial'];
   public $property_types = ['Apartment', 'Condominium', 'House', 'Townhouse', 'Commercial', 'Industrial'];
   public $selected_ptype = '';
   public $selected_utype = '';
-
-
-
-
+  public $unitLocation = '';
+  public $propLocation = '';
 
   public function render()
   {
     return view('frontend.partials.search-bar');
+  }
+
+  public function searchProperty()
+  {
+  }
+  public function unitProperty()
+  {
   }
 }
