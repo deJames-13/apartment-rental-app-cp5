32e1cc842b19c73fb285434531413621fbe290ab
Hisui3003 <yatogod22@gmail.com>
2024-04-02 00:25:37 +0800

    komint

--- resources/views/livewire/table-view-units.blade.php
+++ resources/views/livewire/table-view-units.blade.php
@@ -1,14 +1,18 @@
 <div>
   @php
-  $users = App\Models\User::with('city')->take(5)->get();
+  $units = App\Models\Unit::all();
 
   $headers = [
-      ['key' => 'id', 'label' => '#'],
-      ['key' => 'name', 'label' => 'Nice Name'],
-      ['key' => 'city.name', 'label' => 'City'] # <---- nested attributes
-  ];
+    ['key' => 'id', 'label' => '#'],
+    ['key' => 'unit_code', 'label' => 'Unit Code'],
+    ['key' => 'room_number', 'label' => 'Room Number'],
+    ['key' => 'floor_number', 'label' => 'Floor Number'],
+    ['key' => 'no_of_bedroom', 'label' => 'Bedrooms'],
+    ['key' => 'no_of_bathroom', 'label' => 'Bathrooms'],
+    ['key' => 'status', 'label' => 'Status'],
+];
   @endphp
 
   {{-- You can use any `$wire.METHOD` on `@row-click` --}}
-  <x-table :headers="$headers" :rows="$users" striped @row-click="alert($event.detail.name)" />
+  <x-table :headers="$headers" :rows="$units" striped @row-click="alert($event.detail.name)" />
 </div>
