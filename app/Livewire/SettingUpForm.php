b28dccd62a9f6c0f6838f5c68d3ead71b9480dd6
deJames-13 <de.james013@gmail.com>
2024-04-04 12:05:21 +0800

    sa

--- app/Livewire/SettingUpForm.php
+++ app/Livewire/SettingUpForm.php
@@ -52,53 +52,47 @@ class SettingUpForm extends Component
 
   public function submit()
   {
-    try {
-
-
-      if ($this->tenant_id_card) {
-        $this->tenant_id_card = $this->tenant_id_card->store('tenant_id_cards', 'public');
-      }
-      if ($this->tenant_signature) {
-        $this->tenant_signature = $this->tenant_signature->store('tenant_signatures', 'public');
-      }
-
-
-      $this->landlord_id = $this->property->landlord_id;
-      $this->tenant_id = auth()->id();
-      $this->status = 'pending';
-
-
-
-      LeaseApplication::create([
-        'tenant_id' => $this->tenant_id,
-        'landlord_id' => $this->landlord_id,
-        'property_id' => $this->property_id,
-        'unit_id' => $this->unit_id,
-        'rent_amount' => $this->rent_amount,
-        'status' => $this->status,
-        'title' => $this->title,
-        'notes' => $this->notes,
-        'tenant_id_card' => $this->tenant_id_card,
-        'tenant_signature' => $this->tenant_signature,
-      ]);
-
-
-      session()->flash('message', 'Application submitted successfully!');
-      $this->reset();
-      $user = auth()->user();
-      Mail::to($user->email)->send(new PendingApplication($user));
-      $this->toast(
-        type: 'success',
-        title: 'Created successfully',
-        description: null,
-        position: 'toast-top toast-end',
-        icon: 'o-information-circle',
-        css: 'alert-success',
-        timeout: 3000,
-      );
-    } catch (\Throwable $th) {
-      dd($th);
+    if ($this->tenant_id_card) {
+      $this->tenant_id_card = $this->tenant_id_card->store('tenant_id_cards', 'public');
     }
+    if ($this->tenant_signature) {
+      $this->tenant_signature = $this->tenant_signature->store('tenant_signatures', 'public');
+    }
+
+
+    $this->landlord_id = $this->property->landlord_id;
+    $this->tenant_id = auth()->id();
+    $this->status = 'pending';
+
+
+
+    LeaseApplication::create([
+      'tenant_id' => $this->tenant_id,
+      'landlord_id' => $this->landlord_id,
+      'property_id' => $this->property_id,
+      'unit_id' => $this->unit_id,
+      'rent_amount' => $this->rent_amount,
+      'status' => $this->status,
+      'title' => $this->title,
+      'notes' => $this->notes,
+      'tenant_id_card' => $this->tenant_id_card,
+      'tenant_signature' => $this->tenant_signature,
+    ]);
+
+
+    session()->flash('message', 'Application submitted successfully!');
+    $this->reset();
+    $user = auth()->user();
+    Mail::to($user->email)->send(new PendingApplication($user));
+    $this->toast(
+      type: 'success',
+      title: 'Created successfully',
+      description: null,
+      position: 'toast-top toast-end',
+      icon: 'o-information-circle',
+      css: 'alert-success',
+      timeout: 3000,
+    );
 
     return redirect()->route('applications.index');
   }
